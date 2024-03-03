3751d3628df810c1306dcbe5fe2537641d217647
Andrew Schreiber <schreiaj@gmail.com>
2024-03-02 23:41:27 -0600

    Slight refactor to create a ShotState class that holds heading and arm pos
    Allows computing where target will be given we maintain a constant
    velocity and offsets accordingly.

---
+++ src/main/java/frc/robot/utils/ShotState.java
@@ -0,0 +1,75 @@
+package frc.robot.utils;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.Constants;
+
+public class ShotState {
+  private final Rotation2d _heading;
+  private final Rotation2d _armAngle;
+  private final double _shooterSpeed;
+  private static final InterpolatingDoubleTreeMap armAngleInterpolation =
+      new InterpolatingDoubleTreeMap();
+
+  public ShotState(Rotation2d heading, Rotation2d armAngle, double shooterSpeed) {
+    _heading = heading;
+    _armAngle = armAngle;
+    _shooterSpeed = shooterSpeed;
+  }
+
+  public Rotation2d get_armAngle() {
+    return _armAngle;
+  }
+
+  public Rotation2d get_heading() {
+    return _heading;
+  }
+
+  public double get_shooterSpeed() {
+    return _shooterSpeed;
+  }
+
+  public static ShotState computedFromPose(
+      Translation2d shotTarget,
+      double rangeToTarget,
+      Pose2d robotPose,
+      ChassisSpeeds robotVel,
+      double dt) {
+    // This computes the distance we will travel (in meters) in the prediction time
+    ChassisSpeeds robotPoseDelta = robotVel.times(dt);
+    Translation2d offset =
+        new Translation2d(robotPoseDelta.vxMetersPerSecond, robotPoseDelta.vyMetersPerSecond);
+
+    // Now we need to offset the target point by that
+    // Note, minus is correct, we want to lead the shot so we figure out where the robot will be in
+    // the future and place the target accordingly (I think)
+    Translation2d futureShotTarget = shotTarget.minus(offset);
+    // Then we extract the robot translation
+    Translation2d robotPosition = robotPose.getTranslation();
+    // Then build the triangle
+    Translation2d triangle = futureShotTarget.minus(robotPosition);
+    // Then use the hypotenuse to compute the angle
+    // The angle is the arctan of the opposite over adjacent
+    Rotation2d chassisAngle = new Rotation2d(triangle.getX(), triangle.getY());
+
+    // Now build a new ShotState object
+    return new ShotState(
+        chassisAngle,
+        Rotation2d.fromRadians(armAngleInterpolation.get(rangeToTarget)),
+        Constants.Arm.Shooter.SHOOTER_SPEED);
+  }
+
+  private void initializeInterpolator() {
+    armAngleInterpolation.put(0.0, 0.0);
+    armAngleInterpolation.put(45.0, -0.232);
+    armAngleInterpolation.put(81.5, -0.47);
+    armAngleInterpolation.put(102.0, -0.58);
+    armAngleInterpolation.put(138.0, -0.65);
+    armAngleInterpolation.put(174.0, -0.66);
+    armAngleInterpolation.put(203.0, -0.67);
+    armAngleInterpolation.put(235.0, -0.68);
+  }
+}
