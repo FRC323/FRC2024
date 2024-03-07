7a8d4c05c22d38009747cf1bdbd61e8f335f4de1
KingoftheFlame <matthew.l.mcclure186@gmail.com>
2024-03-07 16:34:06 -0600

    Fixed Blue Shooter Target Position

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -62,7 +62,7 @@ public final class Constants {
     public static final double LIMELIGHT_MOUNT_ANGLE_DEGREES = 25.52;
     public static final double LIMELIGHT_LENS_HEIGHT_INCHES = 18.17;
     public static final Translation2d RED_SHOT_TARGET =  new Translation2d(16.40,5.52);
-    public static final Translation2d BLUE_SHOT_TARGET = new Translation2d(0.54,5.43);
+    public static final Translation2d BLUE_SHOT_TARGET = new Translation2d(0.0,5.52);
   }
 
   public static final int SPARK_INIT_RETRY_ATTEMPTS = 5;
