52117b7efd1961a731b62384bb9602df73265533
KingoftheFlame <matthew.l.mcclure186@gmail.com>
2024-03-20 17:18:16 -0500

    Made Shoot Button also spinup shooter

--- src/main/java/frc/robot/commands/ButtonCommands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ButtonCommands/ShootCommand.java
@@ -1,16 +1,26 @@
 package frc.robot.commands.ButtonCommands;
 
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.Constants;
+import frc.robot.Constants.Shooter;
 import frc.robot.commands.SetCommands.SetFeederSpeed;
+import frc.robot.commands.SetCommands.SetShooterSpeed;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShootCommand extends SequentialCommandGroup{
     public ShootCommand(FeederSubsystem feederSubsystem,ShooterSubsystem shooterSubsystem){
         addCommands(
-            new WaitUntilCommand(() -> shooterSubsystem.atShootSpeed()),
+            new ParallelRaceGroup(
+                new SetShooterSpeed(shooterSubsystem, Shooter.SHOOTER_SPEED),
+                new WaitCommand(0.5)
+            ),
             new SetFeederSpeed(feederSubsystem, Constants.Feeder.FEED_SHOOT_SPEED),
             new WaitUntilCommand(() -> !feederSubsystem.isHoldingNote())
         );
